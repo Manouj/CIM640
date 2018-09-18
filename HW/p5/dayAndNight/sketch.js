068dd27eb206634b388d6c3f49ddfbb937eaeeba
Manouj Kumar <mxg1610@miami.edu>
2018-09-18 17:09:08 -0400

    EditJs

--- HW/p5/dayAndNight/sketch.js
+++ HW/p5/dayAndNight/sketch.js
@@ -1,6 +1,17 @@
 var sun;
 var moon;
-var bgColor=0;
+var r = 255;
+var g = 255;
+var b = 255;
+
+var cX = 50;
+var cY = 50;
+var cEdge= 20;
+var rectX = 50;
+var rectY = 75;
+var rectSize = 100;
+
+
 function preload()
 {
     sun = loadImage('assets/sun.png');
@@ -16,25 +27,56 @@ createCanvas(800,400);
 function draw() {
   // put drawing code here
 
+  background(r,g,b);
 
-  background(240);
-  textSize(20);
-  color("red");
-  text("Move over each area",20,20);
-  text("mouseX pos:"+mouseX,20,50);
-  fill(bgColor);
-  rect(0,0,width,height);
   line(width/2,0,width/2,height);
-  if(mouseX<width/2)
+  if(mouseX>=0 && mouseX<width/2)
   {
 
     image(sun,50,100);
-    bgColor = 255;
+
 
   }
-  else if (mouseX>width/2) {
+  else if (mouseX>=width/2) {
+
     image(moon,450,100);
-    bgColor = 000;
+
   }
+
+
+var cDist = dist(cX, cY, mouseX, mouseY);
+
+
+  textSize(20);
+  text("Move over each area",20,20);
+  text("cDist is " + cDist, 20, 60) ;
+  text("mouseX pos:" + mouseX,20,100);
+
+
+  if(cDist < 10)
+  {
+    r=255;
+    g=0;
+    b=0;
+  }
+  else{
+    r = 255;
+    g = 255;
+    b = 255;
+  }
+
+  if (mouseX >= rectX && mouseX < rectX+rectSize)
+  {
+    if (mouseY >= rectY && mouseY < rectY+rectSize)
+    {
+    r=random(255);
+    g=random(255);
+    b=random(255);
+  }
+  }
+
+  fill(r,g,b);
+  ellipse(cX,cY,cEdge,cEdge);
+  rect(rectX,rectY,rectSize,rectSize);
 
 }
